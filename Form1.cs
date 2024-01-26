9434fabc99c4daefb6f0c6fdee3ae9507f7f1bbd
TnhatY <theanh0343@gmailcom>
2024-01-26 13:28:02 +0700

    test

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace do_an
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // hoang
+            //hoang
+        }
     }
 }
