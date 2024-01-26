1fb0025ce6b44a351857851b6d37f64fcef1dc91
TnhatY <theanh0343@gmailcom>
2024-01-26 18:16:00 +0700

    mm

--- Form1.cs
+++ Form1.cs
@@ -19,8 +19,8 @@ namespace do_an
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // tran the anh
-            // tran the anh
+            // 1234
+            // 5234
         }
     }
 }
